056b7882d59fe709febea906913d5fcd71424062
Sergej <55631247+milics7@users.noreply.github.com>
2024-04-26 20:06:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="srb">
+<html lang="rs">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TEST</title>
+    <title>HOMEWORK 06</title>
 </head>
 <body>
 
